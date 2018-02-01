045ebb763733b1796e0ef4a1b460161462eeb3d1
m.ito <m.ito@conol.jp>
2018-02-01 17:13:51 +0900

    Favorライブラリを追加

---
+++ CUONA/Favor/Favor.swift
@@ -0,0 +1,15 @@
+//
+//  Favor.swift
+//  Favor
+//
+//  Created by Masafumi Ito on 2018/02/01.
+//  Copyright © 2018年 conol, Inc. All rights reserved.
+//
+
+import UIKit
+import CUONACore
+
+@available(iOS 11.0, *)
+public class Favor: NSObject, CUONAManagerDelegate, DeviceManagerDelegate
+{
+}
