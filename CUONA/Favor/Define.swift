f392bcf0e6ab388017fa9f9eab80f49151b0d28e
m.ito <m.ito@conol.jp>
2018-02-06 14:32:01 +0900

    Favorで使用する定数をまとめたDefineファイルを作成

---
+++ CUONA/Favor/Define.swift
@@ -0,0 +1,14 @@
+//
+//  Define.swift
+//  Favor
+//
+//  Created by Masafumi Ito on 2018/02/06.
+//  Copyright © 2018年 conol, Inc. All rights reserved.
+//
+
+import UIKit
+
+struct ApiUrl {
+    static let endPoint = "http://favor-dev.cuona.io"
+    static let registerUesr = endPoint + "/api/users/register.json"
+}
