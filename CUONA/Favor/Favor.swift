9e8799725fb0dfc805adef553f1974134e025a71
m.ito <m.ito@conol.jp>
2018-02-19 15:03:09 +0900

    Menuモデル作成

--- CUONA/Favor/Favor.swift
+++ CUONA/Favor/Favor.swift
@@ -148,6 +148,48 @@ public class Shop: NSObject
     }
 }
 
+public class Menu: NSObject
+{
+    var id:Int = 0
+    var category_id:Int = 0
+    
+    public private(set) var name:String = ""
+    public private(set) var price_cents:Int = 0
+    public private(set) var price_format:String = ""
+    public private(set) var notes:String? = nil
+    public private(set) var images:[Image?] = []
+    public private(set) var category_name:String = ""
+    
+    init(jsonData: [String : Any]) {
+        
+        // 各メンバ変数に値を設定
+        self.id            = jsonData["id"] as! Int
+        self.category_id   = jsonData["category_id"] as! Int
+        self.name          = jsonData["name"] as! String
+        self.price_cents   = jsonData["price_cents"] as! Int
+        self.price_format  = jsonData["price_format"] as! String
+        self.notes         = jsonData["notes"] as? String
+        self.category_name = jsonData["category_name"] as! String
+        
+        // imagesの情報を設定
+        for imageJson in jsonData["images"] as! [[String : Any]]
+        {
+            self.images.append(Image(imageJson))
+        }
+    }
+    
+    public class Image
+    {
+        public private(set) var image_url = ""
+        
+        init(_ imageJson: [String : Any])
+        {
+            self.image_url = imageJson["image_url"] as! String
+        }
+    }
+}
+
+
 @objc public protocol FavorDelegate: class
 {
     // ユーザー登録
